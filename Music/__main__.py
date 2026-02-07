978051a34513943954a6ee1548871605f9d5f872
hccccrr <vrajiv830@gmail.com>
2026-02-07 15:24:43 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -71,5 +71,8 @@ async def start_bot():
     )
 
 
+import asyncio
+
 if __name__ == "__main__":
-    hellbot.(start_bot())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_bot())
