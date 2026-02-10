52bc3cccafe8c1162dfe33d88d1738a82bf8092c
hccccrr <vrajiv830@gmail.com>
2026-02-10 15:06:38 +0530

    V3

--- Music/version.py
+++ Music/version.py
@@ -1,14 +1,14 @@
 import platform
 import time
 
-from pyrogram import __version__ as pyro_version
+from telethon import __version__ as telethon_version
 from pytgcalls.__version__ import __version__ as pytgcalls_version
 
 # versions dictionary
 __version__ = {
     "Hell Music": "3.0.0",
     "Python": platform.python_version(),
-    "Pyrogram": pyro_version,
+    "Telethon": telethon_version,
     "PyTgCalls": pytgcalls_version,
 }
 
