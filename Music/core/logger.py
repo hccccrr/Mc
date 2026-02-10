01e9668af658872a6e6fbc5e1882b972a4bd1023
hccccrr <vrajiv830@gmail.com>
2026-02-10 15:19:52 +0530

    V3

--- Music/core/logger.py
+++ Music/core/logger.py
@@ -14,7 +14,7 @@ logging.basicConfig(
 )
 
 
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("telethon").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
 
