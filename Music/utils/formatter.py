902c1daea9619d06212658b3c691772d11079018
hccccrr <vrajiv830@gmail.com>
2026-02-10 09:30:21 +0530

    Create formatter.py

---
+++ Music/utils/formatter.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from pyrogram.types import Message
+
+
+def secs_to_mins(seconds):
+    if seconds is not None:
+        seconds = int(seconds)
+        d, h, m, s = (
+            seconds // (3600 * 24),
+            seconds // 3600 % 24,
+            seconds % 3600 // 60,
+            seconds % 3600 % 60,
+        )
+        if d > 0:
+            return "{:02d}:{:02d}:{:02d}:{:02d}".format(d, h, m, s)
+        elif h > 0:
+            return "{:02d}:{:02d}:{:02d}".format(h, m, s)
+        elif m > 0:
+            return "{:02d}:{:02d}".format(m, s)
+        elif s > 0:
+            return "00:{:02d}".format(s)
+    return "-"
