1f4835a36fb181da2c995530afc50e634bf86f34
hccccrr <vrajiv830@gmail.com>
2026-02-10 15:06:00 +0530

    V3

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,5 +1,3 @@
-from pyrogram import idle
-
 from config import Config
 from Music.core.calls import hellmusic
 from Music.core.clients import hellbot
@@ -13,12 +11,10 @@ from Music.version import __version__
 async def start_bot():
     hmusic_version = __version__["Hell Music"]
     py_version = __version__["Python"]
-    pyro_version = __version__["Pyrogram"]
+    telethon_version = __version__["Telethon"]
     pycalls_version = __version__["PyTgCalls"]
 
-    LOGS.info(
-        "\x41\x6c\x6c\x20\x43\x68\x65\x63\x6b\x73\x20\x43\x6f\x6d\x70\x6c\x65\x74\x65\x64\x21\x20\x4c\x65\x74\x27\x73\x20\x53\x74\x61\x72\x74\x20\x48\x65\x6c\x6c\x2d\x4d\x75\x73\x69\x63\x2e\x2e\x2e"
-    )
+    LOGS.info("All Checks Completed! Let's Start Hell-Music...")
 
     await user_data.setup()
     await hellbot.start()
@@ -27,16 +23,16 @@ async def start_bot():
 
     try:
         if Config.BOT_PIC:
-            await hellbot.app.send_photo(
+            await hellbot.app.send_file(
                 int(Config.LOGGER_ID),
                 Config.BOT_PIC,
-                TEXTS.BOOTED.format(
+                caption=TEXTS.BOOTED.format(
                     Config.BOT_NAME,
                     hmusic_version,
                     py_version,
-                    pyro_version,
+                    telethon_version,
                     pycalls_version,
-                    hellbot.app.mention(style="md"),
+                    f"@{hellbot.app.me.username}",
                 ),
             )
         else:
@@ -46,29 +42,24 @@ async def start_bot():
                     Config.BOT_NAME,
                     hmusic_version,
                     py_version,
-                    pyro_version,
+                    telethon_version,
                     pycalls_version,
-                    hellbot.app.mention(style="md"),
+                    f"@{hellbot.app.me.username}",
                 ),
             )
     except Exception as e:
-        LOGS.warning(
-            f"\x45\x72\x72\x6f\x72\x20\x69\x6e\x20\x4c\x6f\x67\x67\x65\x72\x3a\x20{e}"
-        )
+        LOGS.warning(f"Error in Logger: {e}")
 
-    LOGS.info(
-        f"\x3e\x3e\x20\x48\x65\x6c\x6c\x2d\x4d\x75\x73\x69\x63\x20\x5b{hmusic_version}\x5d\x20\x69\x73\x20\x6e\x6f\x77\x20\x6f\x6e\x6c\x69\x6e\x65\x21"
-    )
+    LOGS.info(f">> Hell-Music [{hmusic_version}] is now online!")
 
-    await idle()
+    # Run until disconnected
+    await hellbot.app.run_until_disconnected()
 
     await hellbot.app.send_message(
         Config.LOGGER_ID,
-        f"\x23\x53\x54\x4f\x50\n\n**\x48\x65\x6c\x6c\x2d\x4d\x75\x73\x69\x63\x20\x5b{hmusic_version}\x5d\x20\x69\x73\x20\x6e\x6f\x77\x20\x6f\x66\x66\x6c\x69\x6e\x65\x21**",
+        f"#STOP\n\n**Hell-Music [{hmusic_version}] is now offline!**",
     )
-    LOGS.info(
-        f"\x48\x65\x6c\x6c\x2d\x4d\x75\x73\x69\x63\x20\x5b{hmusic_version}\x5d\x20\x69\x73\x20\x6e\x6f\x77\x20\x6f\x66\x66\x6c\x69\x6e\x65\x21"
-    )
+    LOGS.info(f"Hell-Music [{hmusic_version}] is now offline!")
 
 
 import asyncio
