0a1ed711ea683d73188a50c4d5dfcd5df6afb980
hccccrr <vrajiv830@gmail.com>
2026-02-10 15:16:00 +0530

    Update clients.py

--- Music/core/clients.py
+++ Music/core/clients.py
@@ -1,4 +1,6 @@
-from pyrogram import Client
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+from telethon.tl.functions.channels import JoinChannelRequest
 
 from config import Config
 from Music.utils.exceptions import HellBotException
@@ -6,60 +8,64 @@ from Music.utils.exceptions import HellBotException
 from .logger import LOGS
 
 
-class HellClient(Client):
+class HellClient:
     def __init__(self):
-        self.app = Client(
+        # Bot client
+        self.app = TelegramClient(
             "HellMusic",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
-            plugins=dict(root="Music.plugins"),
-            workers=100,
         )
 
-        self.user = Client(
-            "HellClient",
+        # User client (assistant)
+        self.user = TelegramClient(
+            StringSession(Config.STRING_SESSION),
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
-            session_string=Config.HELLBOT_SESSION,
-            no_updates=True,
         )
 
     async def start(self):
-        LOGS.info("\x3e\x3e\x20\x42\x6f\x6f\x74\x69\x6e\x67\x20\x75\x70\x20\x48\x65\x6c\x6c\x4d\x75\x73\x69\x63\x2e\x2e\x2e")
+        LOGS.info(">> Booting up HellMusic...")
+        
         if Config.BOT_TOKEN:
-            await self.app.start()
+            await self.app.start(bot_token=Config.BOT_TOKEN)
             me = await self.app.get_me()
             self.app.id = me.id
-            self.app.mention = me.mention
             self.app.name = me.first_name
             self.app.username = me.username
-            LOGS.info(f"\x3e\x3e\x20{self.app.name}\x20\x69\x73\x20\x6f\x6e\x6c\x69\x6e\x65\x20\x6e\x6f\x77\x21")
-        if Config.HELLBOT_SESSION:
+            # Store me object for later use
+            self.app.me = me
+            LOGS.info(f">> {self.app.name} is online now!")
+        
+        if Config.STRING_SESSION:
             await self.user.start()
             me = await self.user.get_me()
             self.user.id = me.id
-            self.user.mention = me.mention
             self.user.name = me.first_name
             self.user.username = me.username
+            # Store me object for later use
+            self.user.me = me
+            
             try:
-                await self.user.join_chat("Its_HellBot")
-                await self.user.join_chat("https://t.me/joinchat/LUzuM9rrEdIwZTFl")
+                await self.user(JoinChannelRequest("https://t.me/PBX_UPDATE"))
+                await self.user(JoinChannelRequest("https://t.me/PBX_CHAT"))
             except:
                 pass
-            LOGS.info(f"\x3e\x3e\x20{self.user.name}\x20\x69\x73\x20\x6f\x6e\x6c\x69\x6e\x65\x20\x6e\x6f\x77\x21")
-        LOGS.info("\x3e\x3e\x20\x42\x6f\x6f\x74\x65\x64\x20\x75\x70\x20\x48\x65\x6c\x6c\x4d\x75\x73\x69\x63\x21")
+            
+            LOGS.info(f">> {self.user.name} is online now!")
+        
+        LOGS.info(">> Booted up HellMusic!")
 
     async def logit(self, hash: str, log: str, file: str = None):
         log_text = f"#{hash.upper()} \n\n{log}"
         try:
             if file:
-                await self.app.send_document(
+                await self.app.send_file(
                     Config.LOGGER_ID, file, caption=log_text
                 )
             else:
                 await self.app.send_message(
-                    Config.LOGGER_ID, log_text, disable_web_page_preview=True
+                    Config.LOGGER_ID, log_text, link_preview=False
                 )
         except Exception as e:
             raise HellBotException(f"[HellBotException]: {e}")
