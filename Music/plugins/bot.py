924a9d3ab505af96b6f625b4ff3bad77e09fdfc7
hccccrr <vrajiv830@gmail.com>
2026-02-11 09:32:03 +0530

    V3

--- Music/plugins/bot.py
+++ Music/plugins/bot.py
@@ -1,8 +1,6 @@
 import datetime
 
-from pyrogram import filters
-from pyrogram.enums import ChatType
-from pyrogram.types import InlineKeyboardMarkup, Message
+from telethon import events
 
 from config import Config
 from Music.core.calls import hellmusic
@@ -16,12 +14,17 @@ from Music.helpers.users import MusicUser
 from Music.utils.youtube import ytube
 
 
-@hellbot.app.on_message(filters.command(["start", "alive"]) & ~Config.BANNED_USERS)
+@hellbot.app.on(events.NewMessage(pattern=r"^/(start|alive)"))
 @check_mode
-async def start(_, message: Message):
-    if message.chat.type == ChatType.PRIVATE:
-        if len(message.command) > 1:
-            deep_cmd = message.text.split(None, 1)[1]
+async def start(event):
+    if event.sender_id in Config.BANNED_USERS:
+        return
+    
+    if event.is_private:
+        parts = event.text.split()
+        if len(parts) > 1:
+            deep_cmd = parts[1]
+            
             if deep_cmd.startswith("song"):
                 results = await ytube.get_data(deep_cmd.split("_", 1)[1], True)
                 about = TEXTS.ABOUT_SONG.format(
@@ -30,19 +33,18 @@ async def start(_, message: Message):
                     results[0]["published"],
                     results[0]["views"],
                     results[0]["duration"],
-                    hellbot.app.mention,
+                    f"@{hellbot.app.me.username}",
                 )
-                await message.reply_photo(
-                    results[0]["thumbnail"],
-                    caption=about,
-                    reply_markup=InlineKeyboardMarkup(
-                        Buttons.song_details_markup(
-                            results[0]["link"],
-                            results[0]["ch_link"],
-                        )
-                    ),
+                await event.reply(
+                    about,
+                    file=results[0]["thumbnail"],
+                    buttons=Buttons.song_details_markup(
+                        results[0]["link"],
+                        results[0]["ch_link"],
+                    )
                 )
                 return
+            
             elif deep_cmd.startswith("user"):
                 userid = int(deep_cmd.split("_", 1)[1])
                 userdbs = await db.get_user(userid)
@@ -54,76 +56,89 @@ async def start(_, message: Message):
                     level,
                     songs,
                     userdbs["join_date"],
-                    hellbot.app.mention,
+                    f"@{hellbot.app.me.username}",
                 )
-                await message.reply_text(
+                await event.reply(
                     to_send,
-                    reply_markup=InlineKeyboardMarkup(Buttons.close_markup()),
-                    disable_web_page_preview=True,
+                    buttons=Buttons.close_markup(),
+                    link_preview=False,
                 )
                 return
+            
             elif deep_cmd.startswith("help"):
-                await message.reply_text(
-                    TEXTS.HELP_PM.format(hellbot.app.mention),
-                    reply_markup=InlineKeyboardMarkup(Buttons.help_pm_markup()),
+                await event.reply(
+                    TEXTS.HELP_PM.format(f"@{hellbot.app.me.username}"),
+                    buttons=Buttons.help_pm_markup(),
                 )
                 return
-        await message.reply_text(
+        
+        sender = await event.get_sender()
+        await event.reply(
             TEXTS.START_PM.format(
-                message.from_user.first_name,
-                hellbot.app.mention,
-                hellbot.app.username,
+                sender.first_name,
+                f"@{hellbot.app.me.username}",
+                hellbot.app.me.username,
             ),
-            reply_markup=InlineKeyboardMarkup(Buttons.start_pm_markup(hellbot.app.username)),
-            disable_web_page_preview=True,
+            buttons=Buttons.start_pm_markup(hellbot.app.me.username),
+            link_preview=False,
         )
-    elif message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-        await message.reply_text(TEXTS.START_GC)
+    
+    elif event.is_group:
+        await event.reply(TEXTS.START_GC)
 
 
-@hellbot.app.on_message(filters.command("help") & ~Config.BANNED_USERS)
-async def help(_, message: Message):
-    if message.chat.type == ChatType.PRIVATE:
-        await message.reply_text(
-            TEXTS.HELP_PM.format(hellbot.app.mention),
-            reply_markup=InlineKeyboardMarkup(Buttons.help_pm_markup()),
+@hellbot.app.on(events.NewMessage(pattern=r"^/help"))
+async def help_cmd(event):
+    if event.sender_id in Config.BANNED_USERS:
+        return
+    
+    if event.is_private:
+        await event.reply(
+            TEXTS.HELP_PM.format(f"@{hellbot.app.me.username}"),
+            buttons=Buttons.help_pm_markup(),
         )
-    elif message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-        await message.reply_text(
+    elif event.is_group:
+        await event.reply(
             TEXTS.HELP_GC,
-            reply_markup=InlineKeyboardMarkup(
-                Buttons.help_gc_markup(hellbot.app.username)
-            ),
+            buttons=Buttons.help_gc_markup(hellbot.app.me.username)
         )
 
 
-@hellbot.app.on_message(filters.command("ping") & ~Config.BANNED_USERS)
-async def ping(_, message: Message):
+@hellbot.app.on(events.NewMessage(pattern=r"^/ping"))
+async def ping(event):
+    if event.sender_id in Config.BANNED_USERS:
+        return
+    
     start_time = datetime.datetime.now()
-    hell = await message.reply_text("Pong!")
+    hell = await event.reply("Pong!")
     calls_ping = await hellmusic.ping()
     stats = await formatter.system_stats()
     end_time = (datetime.datetime.now() - start_time).microseconds / 1000
-    await hell.edit_text(
+    
+    await hell.edit(
         TEXTS.PING_REPLY.format(end_time, stats["uptime"], calls_ping),
-        disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(Buttons.close_markup()),
+        link_preview=False,
+        buttons=Buttons.close_markup(),
     )
 
 
-@hellbot.app.on_message(filters.command("sysinfo") & ~Config.BANNED_USERS)
+@hellbot.app.on(events.NewMessage(pattern=r"^/sysinfo"))
 @check_mode
 @UserWrapper
-async def sysinfo(_, message: Message):
+async def sysinfo(event):
+    if event.sender_id in Config.BANNED_USERS:
+        return
+    
     stats = await formatter.system_stats()
-    await message.reply_text(
+    await event.reply(
         TEXTS.SYSTEM.format(
             stats["core"],
             stats["cpu"],
             stats["disk"],
             stats["ram"],
             stats["uptime"],
-            hellbot.app.mention,
+            f"@{hellbot.app.me.username}",
         ),
-        reply_markup=InlineKeyboardMarkup(Buttons.close_markup()),
-    )
+        buttons=Buttons.close_markup(),
+)
+    
