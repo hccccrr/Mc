1a71cc6e845c2b67d4b8d5c9ed22cffbc56cccba
hccccrr <vrajiv830@gmail.com>
2026-02-12 14:48:59 +0530

    V3

--- Music/plugins/favorites.py
+++ Music/plugins/favorites.py
@@ -1,8 +1,7 @@
 import datetime
 import random
 
-from pyrogram import filters
-from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, Message
+from telethon import events
 
 from config import Config
 from Music.core.clients import hellbot
@@ -15,36 +14,51 @@ from Music.utils.play import player
 from Music.utils.youtube import ytube
 
 
-@hellbot.app.on_message(
-    filters.command(["favs", "myfavs", "favorites", "delfavs"]) & ~Config.BANNED_USERS
-)
+@hellbot.app.on(events.NewMessage(pattern=r"^/(favs|myfavs|favorites|delfavs)"))
 @check_mode
 @UserWrapper
-async def favorites(_, message: Message):
+async def favorites(event):
+    if event.sender_id in Config.BANNED_USERS:
+        return
+    
     delete = False
-    hell = await message.reply_text("Fetching favorites ...")
-    favs = await db.get_all_favorites(message.from_user.id)
+    hell = await event.reply("Fetching favorites ...")
+    favs = await db.get_all_favorites(event.sender_id)
+    
     if not favs:
-        await hell.edit_text("You dont have any favorite tracks added to the bot!")
-    if message.command[0][0] == "d":
+        return await hell.edit("You dont have any favorite tracks added to the bot!")
+    
+    cmd = event.text.split()[0]
+    if cmd[1] == "d":
         delete = True
+    
+    sender = await event.get_sender()
+    mention = f"[{sender.first_name}](tg://user?id={sender.id})"
+    
     await MakePages.favorite_page(
-        hell, favs, message.from_user.id, message.from_user.mention, 0, 0, True, delete
+        hell, favs, event.sender_id, mention, 0, 0, True, delete
     )
 
 
-@hellbot.app.on_callback_query(filters.regex(r"add_favorite") & ~Config.BANNED_USERS)
-async def add_favorites(_, cb: CallbackQuery):
-    _, video_id = cb.data.split("|")
-    track = await db.get_favorite(cb.from_user.id, video_id)
+@hellbot.app.on(events.CallbackQuery(pattern=b"add_favorite"))
+async def add_favorites(event):
+    if event.sender_id in Config.BANNED_USERS:
+        return
+    
+    data = event.data.decode().split("|")
+    _, video_id = data
+    
+    track = await db.get_favorite(event.sender_id, video_id)
     if track:
-        return await cb.answer("Already in your favorites!", show_alert=True)
-    count = len(await db.get_all_favorites(cb.from_user.id))
+        return await event.answer("Already in your favorites!", alert=True)
+    
+    count = len(await db.get_all_favorites(event.sender_id))
     if count == Config.MAX_FAVORITES:
-        return await cb.answer(
+        return await event.answer(
             f"You can't have more than {Config.MAX_FAVORITES} favorites!",
-            show_alert=True,
+            alert=True,
         )
+    
     details = await ytube.get_data(video_id, True)
     context = {
         "video_id": details[0]["id"],
@@ -52,46 +66,56 @@ async def add_favorites(_, cb: CallbackQuery):
         "duration": details[0]["duration"],
         "add_date": datetime.datetime.now().strftime("%d-%m-%Y %H:%M"),
     }
-    await db.add_favorites(cb.from_user.id, video_id, context)
-    await cb.answer(
-        f"Added to your favorites!\n\n{details[0]['title'][:50]}", show_alert=True
+    await db.add_favorites(event.sender_id, video_id, context)
+    await event.answer(
+        f"Added to your favorites!\n\n{details[0]['title'][:50]}", alert=True
     )
 
 
-@hellbot.app.on_callback_query(filters.regex(r"myfavs") & ~Config.BANNED_USERS)
-async def myfavs_cb(_, cb: CallbackQuery):
-    _, action, user_id, page, delete = cb.data.split("|")
-    if int(user_id) != cb.from_user.id:
-        return await cb.answer("This is not for you!", show_alert=True)
+@hellbot.app.on(events.CallbackQuery(pattern=b"myfavs"))
+async def myfavs_cb(event):
+    if event.sender_id in Config.BANNED_USERS:
+        return
+    
+    data = event.data.decode().split("|")
+    _, action, user_id, page, delete = data
+    
+    if int(user_id) != event.sender_id:
+        return await event.answer("This is not for you!", alert=True)
+    
     if action == "close":
-        await cb.message.delete()
-        await cb.answer("Closed!", show_alert=True)
+        await event.delete()
+        await event.answer("Closed!", alert=True)
     elif action == "play":
         btns = Buttons.playfavs_markup(int(user_id))
-        await cb.message.edit_text(
+        await event.edit(
             "**❤️ Favorites Play:** \n\n__Choose the stream type to play your favorites list:__",
-            reply_markup=InlineKeyboardMarkup(btns),
+            buttons=btns,
         )
     else:
         collection = await db.get_all_favorites(int(user_id))
         last_page, _ = formatter.group_the_list(collection, 5, length=True)
         last_page -= 1
-        if int(page) == 0 and action == "prev":
+        
+        page = int(page)
+        if page == 0 and action == "prev":
             new_page = last_page
-        elif int(page) == last_page and action == "next":
+        elif page == last_page and action == "next":
             new_page = 0
         else:
-            new_page = int(page) + 1 if action == "next" else int(page) - 1
+            new_page = page + 1 if action == "next" else page - 1
+        
         index = new_page * 5
-        if int(delete) == 1:
-            to_del = False
-        else:
-            to_del = True
+        to_del = False if int(delete) == 1 else True
+        
+        sender = await event.get_sender()
+        mention = f"[{sender.first_name}](tg://user?id={sender.id})"
+        
         await MakePages.favorite_page(
-            cb,
+            event,
             collection,
             int(user_id),
-            cb.from_user.mention,
+            mention,
             new_page,
             index,
             True,
@@ -99,43 +123,62 @@ async def myfavs_cb(_, cb: CallbackQuery):
         )
 
 
-@hellbot.app.on_callback_query(filters.regex(r"delfavs") & ~Config.BANNED_USERS)
-async def delfavs_cb(_, cb: CallbackQuery):
-    _, action, user_id = cb.data.split("|")
-    if int(user_id) != cb.from_user.id:
-        return await cb.answer("This is not for you!", show_alert=True)
+@hellbot.app.on(events.CallbackQuery(pattern=b"delfavs"))
+async def delfavs_cb(event):
+    if event.sender_id in Config.BANNED_USERS:
+        return
+    
+    data = event.data.decode().split("|")
+    _, action, user_id = data
+    
+    if int(user_id) != event.sender_id:
+        return await event.answer("This is not for you!", alert=True)
+    
     collection = await db.get_all_favorites(int(user_id))
+    
     if action == "all":
         for i in collection:
             await db.rem_favorites(int(user_id), i["video_id"])
-        return await cb.message.edit_text("Deleted all your favorites!")
+        return await event.edit("Deleted all your favorites!")
     else:
         is_deleted = await db.rem_favorites(int(user_id), action)
         if is_deleted:
-            await cb.answer("Deleted from your favorites!", show_alert=True)
+            await event.answer("Deleted from your favorites!", alert=True)
             collection = await db.get_all_favorites(int(user_id))
+            sender = await event.get_sender()
+            mention = f"[{sender.first_name}](tg://user?id={sender.id})"
             await MakePages.favorite_page(
-                cb, collection, int(user_id), cb.from_user.mention, 0, 0, True, True
+                event, collection, int(user_id), mention, 0, 0, True, True
             )
         else:
-            await cb.answer("Not in your favorites!", show_alert=True)
+            await event.answer("Not in your favorites!", alert=True)
 
 
-@hellbot.app.on_callback_query(filters.regex(r"favsplay") & ~Config.BANNED_USERS)
-async def favsplay_cb(_, cb: CallbackQuery):
-    _, action, user_id = cb.data.split("|")
-    if int(user_id) != cb.from_user.id:
-        return await cb.answer("This is not for you!", show_alert=True)
+@hellbot.app.on(events.CallbackQuery(pattern=b"favsplay"))
+async def favsplay_cb(event):
+    if event.sender_id in Config.BANNED_USERS:
+        return
+    
+    data = event.data.decode().split("|")
+    _, action, user_id = data
+    
+    if int(user_id) != event.sender_id:
+        return await event.answer("This is not for you!", alert=True)
+    
     if action == "close":
-        await cb.message.delete()
-        return await cb.answer("Closed!", show_alert=True)
+        await event.delete()
+        return await event.answer("Closed!", alert=True)
     else:
-        await cb.message.edit_text("Playing your favorites")
+        await event.edit("Playing your favorites")
         all_tracks = await db.get_all_favorites(int(user_id))
         random.shuffle(all_tracks)
         video = True if action == "video" else False
+        
+        sender = await event.get_sender()
+        mention = f"[{sender.first_name}](tg://user?id={sender.id})"
+        
         context = {
-            "user_id": cb.from_user.id,
-            "user_mention": cb.from_user.mention,
+            "user_id": event.sender_id,
+            "user_mention": mention,
         }
-        await player.playlist(cb.message, context, all_tracks, video)
+        await player.playlist(event, context, all_tracks, video)
